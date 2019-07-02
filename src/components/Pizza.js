b1ea4ec0b5c4b612ee07b94da2f88c3edbf1d1af
Won Kim <wonsae.kim@gmail.com>
2019-07-02 18:37:07 -0400

    Done.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const Pizza = () => {
+const Pizza = (props) => {
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{props.topping}</td>
+      <td>{props.size}</td>
+      <td>{(props.vegetarian) ? 'Veggie' : 'Not Veggie'}</td>
+      <td><button type="button" onClick={() => props.handleInfo(props)} className="btn btn-primary">Edit Pizza</button></td>
     </tr>
   )
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+  const [newInfo, setNewInfo] = useState(props.pizzaInfo)
+
+  useEffect(() => {
+    setNewInfo(props.pizzaInfo);
+    console.log(newInfo)
+  }, [props.pizzaInfo])
+
+  const handleChange = (e) => {
+    e.persist()
+    if (e.target.value === 'vegetarian') {
+      setNewInfo({...newInfo, [e.target.value]: !newInfo.vegetarian})
+    } else {
+      setNewInfo({...newInfo, [e.target.name]: e.target.value})
+    }
+  }
+
   return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input type="text" className="form-control" name='topping' placeholder="Pizza Topping" value={newInfo.topping} onChange={(e) => handleChange(e)}/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={newInfo.size} name='size' className="form-control" onChange={(e) => handleChange(e)}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,23 +31,22 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" name='vegetarian' type="radio" value="vegetarian" checked={(newInfo.vegetarian) ? true : false} onChange={(e) => handleChange(e)}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" name='vegetarian' type="radio" value="vegetarian" checked={(newInfo.vegetarian) ? false : true} onChange={(e) => handleChange(e)}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={() => props.editPizza(newInfo)}>Submit</button>
         </div>
       </div>
-
   )
 }
 
